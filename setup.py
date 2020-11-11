3e3e605fbc5a04d647ee7a0d86d5c24609adf80a
Bruno Odinukweze <48985022+Bruno-Odinukweze@users.noreply.github.com>
2020-11-11 11:06:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 setup(
     ...
-    dependency_links=['https://github.com/Bruno-Odinukweze/Accountss.git#egg=Accountss']
+    dependency_links=['https://github.com/Bruno-Odinukweze/Accountss/tarball/master#egg=Accountss']
     ...
 )
