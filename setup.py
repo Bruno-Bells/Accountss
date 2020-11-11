f4788c84de30a734665c8f9aaae1087340d9f301
Bruno Odinukweze <48985022+Bruno-Odinukweze@users.noreply.github.com>
2020-11-11 12:43:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 
 setup(
     name='accountss',
+    version='0.0.1',
     dependency_links=['https://github.com/Bruno-Odinukweze/Accountss/tarball/master#egg=accountss']
 )
